3e49fcb1d6e31a3c6a23221247078a0ec835113b
fedik <getthesite@gmail.com>
2012-04-24 19:56:49 +0300

    auto generation for wrapers works but not for "title","messge" etc .. so, commit before other big changes

---
+++ addfullajax-page.tpl.php
@@ -0,0 +1,18 @@
+<?php
+krumo($variables);
+/**
+ * @file
+ * addfullajax-page.tpl.php
+ *
+ * Available variables:
+ *   all that for page.tpl.php
+ *   +
+ *   $regions = array that contain regions that need render
+ *   $main_region = string, region name that contain main contant
+ */
+
+foreach ($regions as $region) {
+  print '<!-- :ax:flax-' . $region . ':begin: //-->';
+  print render($variables['page'][$region]);
+  print '<!-- :ax:flax-' . $region . ':end: //-->';
+}
