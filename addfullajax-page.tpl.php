f74ae901d97a670c39379d44dbac6f75891623f1
fedik <getthesite@gmail.com>
2012-04-25 11:58:34 +0300

    looks like works well

--- addfullajax-page.tpl.php
+++ addfullajax-page.tpl.php
@@ -1,5 +1,4 @@
 <?php
-krumo($variables);
 /**
  * @file
  * addfullajax-page.tpl.php
@@ -7,13 +6,26 @@ krumo($variables);
  * Available variables:
  *   all that for page.tpl.php
  *   +
- *   $regions = array that contain regions that need render
- *   $main_region = string, region name that contain main contant
- *   $content_id = CSS id of main block content
+ *   $regions = array that contain regions that need render (@see configuration)
+ *   $main_region = string, region name that contain main contant (@see configuration)
+ *   $content_id = CSS id of main block content(@see configuration)
  */
-
-foreach ($regions as $region) {
-  print '<!-- :ax:flax-' . $region . ':begin: //-->';
-  print render($variables['page'][$region]);
-  print '<!-- :ax:flax-' . $region . ':end: //-->';
-}
+?>
+<?php foreach ($regions as $region) : ?>
+  <?php if ($region == $main_region): //render main content ?>
+    <!-- :ax:<?php print $content_id; ?>:begin: //-->
+    <?php print render($title_prefix); ?>
+    <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
+    <?php print render($title_suffix); ?>
+    <?php if ($tabs): ?><div class="tabs"><?php print render($tabs); ?></div><?php endif; ?>
+    <?php print render($page['help']); ?>
+    <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
+    <?php print render($variables['page'][$region]); ?>
+    <?php print $feed_icons; ?>
+    <!-- :ax:<?php print $content_id; ?>:end: //-->
+  <?php else : //render additional blocks ?>
+    <!-- :ax:flax-<?php print $region; ?>:begin: //-->
+    <?php print render($variables['page'][$region]); ?>
+    <!-- :ax:flax-<?php print $region; ?>:end: //-->
+  <?php endif;?>
+<?php endforeach; ?>
