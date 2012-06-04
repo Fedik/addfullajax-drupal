4d8badd947982e3e556199523e4ddab0043f3daa
fedik <getthesite@gmail.com>
2012-06-04 12:01:27 +0300

    clean up code

--- addfullajax-page.tpl.php
+++ addfullajax-page.tpl.php
@@ -6,13 +6,13 @@
  * Available variables:
  *   all that for page.tpl.php
  *   +
- *   $regions = array that contain regions that need render (@see configuration)
- *   $main_region = string, region name that contain main contant (@see configuration)
- *   $content_id = CSS id of main block content(@see configuration)
+ *   $regions = array that contain regions that need render (look at configuration)
+ *   $main_region = string, region name that contain main contant (look at configuration)
+ *   $content_id = CSS id of main block content(look at configuration)
  */
 ?>
 <?php foreach ($regions as $region) : ?>
-  <?php if ($region == $main_region): //render main content ?>
+  <?php if ($region == $main_region): // render main content ?>
     <!-- :ax:<?php print $content_id; ?>:begin: //-->
     <?php print render($title_prefix); ?>
     <?php if ($title): ?><h1 class="title" id="page-title"><?php print $title; ?></h1><?php endif; ?>
@@ -23,7 +23,7 @@
     <?php print render($variables['page'][$region]); ?>
     <?php print $feed_icons; ?>
     <!-- :ax:<?php print $content_id; ?>:end: //-->
-  <?php else : //render additional blocks ?>
+  <?php else : // render additional blocks ?>
     <!-- :ax:flax-<?php print $region; ?>:begin: //-->
     <?php print render($variables['page'][$region]); ?>
     <!-- :ax:flax-<?php print $region; ?>:end: //-->
